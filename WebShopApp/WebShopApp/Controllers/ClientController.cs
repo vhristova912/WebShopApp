a591adfbae77505cf92c4c9b390fec3745da8d3e
unknown <viki66hristova@gmail.com>
2024-01-04 11:37:12 +0200

    Working on p6

---
+++ WebShopApp/WebShopApp/Controllers/ClientController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+using WebShopApp.Infrastucture.Data.Domain;
+using WebShopApp.Models.Client;
+
+namespace WebShopApp.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ClientController(UserManager<ApplicationUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+
+        // GET: ClientController
+        public async Task<IActionResult> Index()
+        {
+            var allUsers = this._userManager.Users.Select(u => new ClientIndexVM
+            {
+                Id = u.Id,
+                UserName = u.UserName,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                Address = u.Address,
+                Email = u.Email,
+            }).ToList();
+            var adminIds = (await _userManager.GetUsersInRoleAsync("Administrator"))
+                .Select(a => a.Id).ToList();
+            foreach (var user in allUsers)
+            {
+                user.IsAdmin = adminIds.Contains(user.Id);
+            }
+            var users=allUsers.Where(x=>x.IsAdmin==false)
+                .OrderBy(x=>x.UserName).ToList();
+            return this.View(users);
+        }
+
+        // GET: ClientController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ClientController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClientController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClientController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ClientController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClientController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ClientController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
