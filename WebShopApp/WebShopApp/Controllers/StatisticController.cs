3ad64cfbdc104a04678a86dd95a2983a8499a2ef
Viktoria Hristova <viki66hristova@gmail.com>
2024-01-05 10:26:40 +0200

    P7 is ready

---
+++ WebShopApp/WebShopApp/Controllers/StatisticController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebShopApp.Core.Contracts;
+using WebShopApp.Models.Statistic;
+
+namespace WebShopApp.Controllers
+{
+    public class StatisticController : Controller
+    {
+        private readonly IStatisticsService _statisticsService;
+
+        public StatisticController(IStatisticsService statisticsService)
+        {
+            this._statisticsService = statisticsService;
+        }
+
+        // GET: StatisticsController
+        public ActionResult Index()
+        {
+            StatisticVM statistics = new StatisticVM();
+            statistics.CountClients = _statisticsService.CountClients();
+            statistics.CountProducts = _statisticsService.CountProducts();
+            statistics.CountOrders= _statisticsService.CountOrders();
+            statistics.SumOrders= _statisticsService.SumOrders();
+            return View(statistics);
+        }
+
+        // GET: StatisticsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StatisticsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StatisticsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StatisticsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StatisticsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StatisticsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StatisticsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- WebShopApp/WebShopApp/Program.cs
+++ WebShopApp/WebShopApp/Program.cs
@@ -38,6 +38,7 @@ namespace WebShopApp
             builder.Services.AddTransient<IBrandService, BrandService>();
             builder.Services.AddTransient<IProductService, ProductService>();
             builder.Services.AddTransient<IOrderService, OrderService>();
+            builder.Services.AddTransient<IStatisticsService, StatisticService>();
 
             var app = builder.Build();
             app.PrepareDatabase();
